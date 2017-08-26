b83c7a76b66deb85e06bde3de64021db00388023
Frederich Munch <marsupial@users.noreply.github.com>
2017-08-26 17:23:18 -0400

    rebuiol

--- bash/packages.sh
+++ bash/packages.sh
@@ -21,7 +21,7 @@ function BotPlatformSetup {
 }
 
 function BotTimeRemaining {
-  local remains=$(expr 42 - $SECONDS / 60)
+  local remains=$(expr 35 - $SECONDS / 60)
   if [[ $remains -gt 0 && ( -z "$1" || $remains -gt $1 ) ]]; then
     echo $remains
   fi
